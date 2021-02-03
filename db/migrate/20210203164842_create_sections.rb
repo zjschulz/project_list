c33ee5d0791cceb0cc63b932b4d60f2d46e654df
zjschulz <zjschulz@gmail.com>
2021-02-03 16:49:22 +0000

    generate m,v,c resources with generators

---
+++ db/migrate/20210203164842_create_sections.rb
@@ -0,0 +1,10 @@
+class CreateSections < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sections do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
