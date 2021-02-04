1cfaa91c09dcb2d339a3315cf7941e08ae31ba97
zjschulz <zjschulz@gmail.com>
2021-02-04 16:33:18 +0000

    update sessions controller and view for logging in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,45 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @user = User.find_by(user_name: params[:user_name])
+        if !@user
+            @error = "Username invalid. Please try again."
+            render :new
+        elsif !@user.authenticate(params[:password])
+            @error = "Password invalid. Please try again."
+            render :new
+        else 
+            log_in(@user)
+            redirect_to welcome_path?
+        end
+        # if request.env["omniauth.auth"]
+        #     @user = User.find_by(github_uid: request.env["omniauth.auth"]["uid"])
+        #     if @user.nil?
+        #         @user = User.create(username: request.env["omniauth.auth"]["info"]["nickname"], github_uid: request.env["omniauth.auth"]["uid"], password: "github")
+        #     end
+        #         log_in(@user)
+        #         redirect_to welcome_path?
+        # else
+        #     @user = User.find_by(username: params[:username])
+        #     if !@user
+        #         @error = "Username invalid. Please try again."
+        #         render :new
+        #     elsif !@user.authenticate(params[:password])
+        #         @error = "Password invalid. Please try again."
+        #         render :new
+        #     else 
+        #         log_in(@user)
+        #         redirect_to volunteers_path
+        #     end
+        # end
+    end
+
+    def destroy
+        session.clear
+        redirect_to login_path
+    end
+
+end
