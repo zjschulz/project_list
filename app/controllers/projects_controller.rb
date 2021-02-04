c9c6a83a4cf1274b805cc1ab4898d1890219b340
zjschulz <zjschulz@gmail.com>
2021-02-04 17:19:16 +0000

    start building projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,26 @@
 class ProjectsController < ApplicationController
+
+    def index
+        @projects = Project.all
+    end
+
+    def new
+        @project = Project.new
+    end
+
+    def create
+        @project = Project.create(project_params)
+        if @project.save
+            redirect_to projects_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:title, :description)
+    end
+
 end
