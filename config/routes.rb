07c98951654f4a1c1a8a932395fa9ec978ddc0dd
zjschulz <zjschulz@gmail.com>
2021-02-04 16:03:49 +0000

    create new user view and complete auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   resources :sections
   resources :comments
   resources :projects
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+
 end
