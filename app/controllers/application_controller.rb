663afc8d15bfc37311b96cbdd57cb49a945e9be8
zjschulz <zjschulz@gmail.com>
2021-02-04 15:47:33 +0000

    set up authorization methods in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+          redirect_to login_path unless logged_in?
+      end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def log_in(bork)
+        session[:user_id] = bork.id
+    end
+
 end
